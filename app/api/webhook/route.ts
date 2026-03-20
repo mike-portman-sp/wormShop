c246babd38a51386c5c4dd42b07e9cc91363bd33
Mike Portman <mikeportmanj@gmail.com>
2026-03-19 22:11:36 -0500

    stripe POST

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,11 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2026-02-25.clover",
-});
-
 export async function POST(req: NextRequest) {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+    apiVersion: "2026-02-25.clover",
+  });
   const body = await req.text();
   const sig = req.headers.get("stripe-signature");
 
